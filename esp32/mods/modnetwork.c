cd74004489c322877b565589db2b81576717e39d
knagymate <knagymate@gmail.com>
2020-02-26 07:50:24 +0100

    Added HTTP_Client module

--- esp32/mods/modnetwork.c
+++ esp32/mods/modnetwork.c
@@ -245,6 +245,7 @@ STATIC const mp_map_elem_t mp_module_network_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_Coap),                (mp_obj_t)&mod_coap },
     { MP_OBJ_NEW_QSTR(MP_QSTR_MDNS),                (mp_obj_t)&mod_mdns },
     { MP_OBJ_NEW_QSTR(MP_QSTR_HTTP_Server),         (mp_obj_t)&mod_http_server },
+	{ MP_OBJ_NEW_QSTR(MP_QSTR_HTTP_Client),         (mp_obj_t)&mod_http_client },
 
 };
 
